30ae1670ca75b61ee661f3ec640007266acf66cc
Mao Takagai <maoam.724@gmail.com>
2022-01-07 21:14:29 +0900

    Initial Commit

---
+++ GettingOffAlarm/GettingOffAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingOffAlarmApp.swift
+//  GettingOffAlarm
+//
+//  Created by mao takagai on 2022/01/07.
+//
+
+import SwiftUI
+
+@main
+struct GettingOffAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
